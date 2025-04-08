0f594c9927def3122857f323735d0d57f15f93bd
Elizandra Waechter <waechterelly@gmail.com>
2025-04-08 12:04:58 -0300

    Alteração do delete.py e main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException, Depends, Body
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional, List, Dict, Any
+import uuid
+import json
+import os
+from datetime import datetime 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title = "API de cadastro de Usuários",
+    descripition = "API para gerenciar usuários e conversar com IA sobre gerenciamento de usuários",
+    version = "1.0.0",
+    
+)
